3ddfe52f4559833cd65a251caf7c8768f7d84ca7
dev@farmadoc.it <l.aliverti@reply.it>
2023-06-24 21:14:27 +0200

    fix cors header

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ async function farmadocInit(el){
     mode: "cors",
     headers:{
       "Content-Type": "application/json",
-      'Access-Control-Allow-Origin': '*'
+      "Access-Control-Allow-Origin": "*"
     }
   }).then(res=>{
     return res.json()
