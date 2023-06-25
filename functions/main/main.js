4c23034302cc5ff1b1fbc7dea60c704d53f0c434
dev@farmadoc.it <l.aliverti@reply.it>
2023-06-25 18:35:27 +0200

    revert

--- functions/main/main.js
+++ functions/main/main.js
@@ -1,32 +1,28 @@
 const faunadb = require('faunadb');
+
 const q = faunadb.query;
+
 const client = new faunadb.Client({
   secret: "fnAE8IkPPKAA15tgIbOo0qtkCNe-ID9bTBmcbWeN",
   domain: 'db.eu.fauna.com'
 });
 
 exports.handler = async (event, context) => {
+
   let clientKey = event.queryStringParameters.key
-  console.log(event?.headers?.origin)
+  
+  console.log(event.headers.origin)
 
   return client.query(
     q.Get(
       q.Match(
-        q.Index('userByKey'),
-        clientKey
+          q.Index('userByKey'),
+          clientKey
       )
     )
-  ).then(res => {
-/*     // return {
-    //   statusCode: 200,
-    //   headers: {
-    //     'Access-Control-Allow-Origin': "*",
-    //     'Access-Control-Allow-Headers': "Content-Type",
-    //     'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify({ authorised: true, res: res.data })
-    // } */
-    if(event?.headers?.origin?.includes(res?.data?.domain)){
+  ).then(res=>{
+
+    if(event.headers.origin.includes(res.data.domain)){
       return{
         statusCode: 200,
         headers: {
@@ -47,5 +43,9 @@ exports.handler = async (event, context) => {
         body: JSON.stringify({authorised: false})
        }
     }
+
+   
+
   })
+
 }
