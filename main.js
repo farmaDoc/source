a396ed98ea43f639a31fdfb120c490de67a0da54
Thomas Simon Mattia <development@puzzlemusicgroup.com>
2023-07-29 13:52:52 +0200

    Update main.js

--- main.js
+++ main.js
@@ -107,17 +107,17 @@ async function farmadocInit(el) {
   let servbtnid = btoa(Math.random().toString()).substring(10, 20);
   let width = screen.width < 960 ? "calc(100% - 20px)" : "500px";
 
-  /* function parseserv(cont){
+  function parseserv(cont){
     console.log(cont)
-    result = cont
+    let servres = cont
     const phoneNumberRegex = /(\+\d{1,2}\s?)?(\(?\d{1,}\)?[\s-]?)?\d{1,}[\s-]?\d{1,}[\s-]?\d{1,}/g;
     const phoneNumbers = cont.match(phoneNumberRegex);
     phoneNumbers.forEach(el=>{
-      result.replace(el,"<a href='tel:"+el+">"+el+"</a>")
+      servres.replace(el,"<a href='tel:"+el+">"+el+"</a>")
     })
-    result = result.replace(/\n/g, "<br>")
-    return(result)
-  } */
+    servres = servres.replace(/\n/g, "<br>")
+    return(servres)
+  }
 
   let modal = `
         <div style="all: unset; background-color: white; box-sizing: border-box; font-family: Arial; z-index: 100000; width: ${width}; position: fixed; bottom: 10px; right: 10px; border: 1px solid grey; border-radius: 10px">
