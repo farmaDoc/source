45a0f0e89c5728070db15b0c853aa8c09a4884e0
Thomas Simon Mattia <development@puzzlemusicgroup.com>
2023-08-08 13:21:33 +0200

    Update sendStats.js

--- functions/sendStats/sendStats.js
+++ functions/sendStats/sendStats.js
@@ -13,30 +13,34 @@ exports.handler = async (event, context) => {
 
     const updateQuery = q.Let(
         {
-          docRef: q.Ref(q.Collection("drugs"), farma),
-          document: q.Get(q.Var("docRef")),
-          myArray: q.Select(["data", "reqs"], q.Var("document")),
+            docRef: q.Ref(q.Collection("drugs"), farma),
+            document: q.Get(q.Var("docRef")),
+            myArray: q.Select(["data", "reqs"], q.Var("document")),
         },
-        q.Console("myArray:", q.Var("myArray")),
-        q.If(
-          q.IsArray(q.Var("myArray")),
-          q.Update(q.Var("docRef"), {
-            data: {
-              my_array: q.Append(q.Var("myArray"), [{
-                ts: now,
-                intent: int
-            }]),
-            },
-          }),
-          q.Update(q.Var("docRef"), {
-            data: {
-              reqs: [{
-                ts: now,
-                intent: int
-            }],
-            },
-          })
+        
+        q.Do(
+            q.Console("myArray:", q.Var("myArray")),
+            q.If(
+                q.IsNull(q.Var("myArray")),
+                q.Update(q.Var("docRef"), {
+                    data: {
+                    my_array: q.Append(q.Var("myArray"), [{
+                        ts: now,
+                        intent: int
+                    }]),
+                    },
+                }),
+                q.Update(q.Var("docRef"), {
+                    data: {
+                    reqs: [{
+                        ts: now,
+                        intent: int
+                    }],
+                    },
+                })
+            )
         )
+        
       );
     
     return client.query(updateQuery).then(res => {
