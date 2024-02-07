2f58decc39521767a95a1599f933606cdb453e5f
Thomas Simon Mattia <development@puzzlemusicgroup.com>
2024-02-07 13:22:17 +0100

    Update main.js

--- main.js
+++ main.js
@@ -315,7 +315,11 @@ async function farmadocInit(el) {
         addRes("in base ai tuoi sintomi, potresti avere bisogno di assistenza per:", true, null)
 
         topmatches.forEach(el=>{
-          document.getElementById(chatid).insertAdjacentHTML("afterbegin", `<span id="farmadoc-int-choice-${el.intent}"'>${el.intent}</span>`);
+          let curDoc = intents.find(
+            (item) => item.ref["@ref"].id == choseInt
+          )
+          let html = `<button id="farmadoc-int-choice-${el.intent}" class="pulsanteDiram" style="cursor: pointer; margin-right: 5px; margin-bottom: 5px; border: none; background-color: #b9b9b9; padding: 10px; border-radius: 10px; display: inline-block; word-wrap: normal; overflow: hidden; position: relative; box-sizing: border-box">${curDoc.name}</button>`
+          document.getElementById(chatid).insertAdjacentHTML("afterbegin", html);
           document.getElementById("farmadoc-int-choice-"+el.intent).addEventListener('click', function() {
             this.style.backgroundColor = "yellow";
           });
