4062fec73b0d8f631868904f78e40a57e34f7cff
Thomas Simon Mattia <development@puzzlemusicgroup.com>
2024-02-07 13:52:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -330,12 +330,9 @@ async function farmadocInit(el) {
           return new Promise(resolve => {
             const waitforCoice = setInterval(() => {
               topmatch.forEach(el=>{
-                let curC = intents.find(
-                  (item) => item.ref["@ref"].id == el.intent
-                )
                 if(document.getElementById("farmadoc-int-choice-"+el.intent).style.backgroundColor == "white"){
                   document.getElementById("buttonrowclear").remove()
-                  document.querySelector('span:contains("in base ai tuoi sintomi, potresti avere bisogno di assistenza per:"').innerText = `in base ai tuoi sintomi, potresti avere bisogno di assistenza per: ${curC.data.title}`
+                  document.getElementById(chatid).querySelector('span').remove()
                   resolve(el.intent)
                   clearInterval(waitforCoice)
                 }
