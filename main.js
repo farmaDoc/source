ea65cb0fabf293236cb15fd145d0d70079286921
Thomas Simon Mattia <admin@toldmagazine.org>
2024-02-26 13:01:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -347,7 +347,7 @@ async function farmadocInit(el) {
                 }
               }
               console.log(el)
-              let htmlC = `<button id="farmadoc-int-choice-${el.intent}"' style="opacity: ${opacity};cursor: pointer; margin-right: 5px; margin-bottom: 5px; border: none; background-color: #b9b9b9; padding: 10px; border-radius: 10px; display: inline-block; word-wrap: normal; overflow: hidden; position: relative; box-sizing: border-box">${curD.data.title}</button>`
+              let htmlC = `<button id="farmadoc-int-choice-${el.intent}"' style="opacity: ${opacity};cursor: pointer; margin-right: 5px; margin-bottom: 5px; border: none; background-color: #b9b9b9; padding: 10px; border-radius: 10px; display: inline-block; word-wrap: normal; overflow: hidden; position: relative; box-sizing: border-box">${curD.data.title} (${(el.probability*100).toFixed(2)}%)</button>`
               if(curD.data.createdBy != "system"){
                 totbtns = totbtns+1
                 document.getElementById("buttonrowclear").insertAdjacentHTML("afterbegin", htmlC);
