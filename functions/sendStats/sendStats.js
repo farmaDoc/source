f860ffc962dcef5265ba5d8f6b0e11b8d4d6418f
Thomas Simon Mattia <admin@toldmagazine.org>
2024-02-26 14:56:15 +0100

    Update sendStats.js

--- functions/sendStats/sendStats.js
+++ functions/sendStats/sendStats.js
@@ -10,6 +10,37 @@ exports.handler = async (event, context) => {
 
     let els = JSON.parse(event.queryStringParameters.obj)
 
+    let promises = []
+    els.forEach(el=>{
+        promises.push(async function(){
+            return await client.query(
+                q.Update(q.Ref(q.Collection('intents'), el[0]), {
+                    data: {
+                        reqs: q.Append(
+                            {
+                                ts: el[2],
+                                farma: el[1]
+                            },
+                            q.Select(
+                                ["data", "reqs"],
+                                q.Get(q.Ref(q.Collection('drugs'), farma))
+                            )
+                        )
+                    }
+                })
+            )
+        })
+    })
+    await Promise.all(promises)
+    return{
+        statusCode: 200,
+        headers: {
+            'Access-Control-Allow-Origin': "*",
+            'Access-Control-Allow-Headers': "Content-Type",
+            'Content-Type': 'application/json'
+        },
+        body: "Success"
+    }
     /* return client.query(
         q.Get(q.Ref(q.Collection('drugs'), farma))
     ).then(res=>{
