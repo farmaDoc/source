bf6b7fcd4432b7b44ab14f9acb17f8b1158576e6
Thomas Simon Mattia <admin@toldmagazine.org>
2024-02-29 15:29:33 +0100

    Update main.js

--- main.js
+++ main.js
@@ -395,8 +395,9 @@ async function farmadocInit(el) {
             console.log(Math.max(...topmatches.map(e=>e.probability)))
             console.log(calculateVariance(topmatches.map(e=>e.probability)))
             console.log(Math.max(...topmatches.map(e=>e.probability))*calculateVariance(topmatches.map(e=>e.probability)))
+            console.log(topmatches.filter(e=>e.probability > Math.max(...topmatches.map(e=>e.probability))*calculateVariance(topmatches.map(e=>e.probability))))
             if(calculateVariance(topmatches.map(e=>e.probability))>0.1){
-              topmatches = topmatches.filter(e=>e.probability > Math.max(...topmatches.map(e=>e.probability))*calculateVariance(topmatches.map(e=>e.probability)))
+              topmatches = topmatches.filter(e=>e.probability > (Math.max(...topmatches.map(e=>e.probability))*calculateVariance(topmatches.map(e=>e.probability))))
             }
             if(topmatches.length < 2 ){
               reject("no matches");
