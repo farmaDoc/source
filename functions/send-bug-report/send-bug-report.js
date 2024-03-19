576db01bbe80207edcff1056e92c83c9c63180c0
Thomas Simon Mattia <admin@toldmagazine.org>
2024-03-19 14:40:12 +0100

    Update send-bug-report.js

--- functions/send-bug-report/send-bug-report.js
+++ functions/send-bug-report/send-bug-report.js
@@ -27,6 +27,11 @@ exports.handler = async (event, context) => {
     ).then(()=>{
         return{
             statusCode: 200,
+            headers: {
+				'Access-Control-Allow-Origin': "*",
+				'Access-Control-Allow-Headers': "Content-Type",
+				'Content-Type': 'application/json'
+			},
         }
     })
 }
