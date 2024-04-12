c1ceac1ad32042e835527a9f9015b5f1a3948e57
Thomas Simon Mattia <admin@toldmagazine.org>
2024-04-12 22:12:26 +0200

    .

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
-window.onerror = function(msg, url, linenumber) {
-  alert('Error message: '+msg+'\nURL: '+url+'\nLine Number: '+linenumber);
-  return true;
-}
 async function farmadocInit(el) {
 
     
